6ca82897afdfbe23a121a1d39e9872886f45cbf1
lipeDaltoe <felipedaltoe9@gmail.com>
2024-04-29 12:25:36 -0300

    Criando a página de listagem de produtos

--- LicaShoppingList/src/app/home/index.js
+++ LicaShoppingList/src/app/home/index.js
@@ -1,11 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  SafeAreaView, 
+  ImageBackground, 
+  TextInput, 
+  TouchableOpacity 
+} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Ionicons }  from '@expo/vector-icons';
 
 export default function Home() {
   return (
-    <View>
-      <Text>Página Inicial</Text>
-    </View>
+    <SafeAreaView style={{ flex:1 }}>
+      <ImageBackground
+        source={require('../../assets/background.jpg')}
+        resizeMode='repeat'
+        style={{flex: 1,justifyContent: 'flex-start'}}
+      >
+        <View style={styles.header}>
+          <Text style={styles.title}>Lista de Produtos</Text>
+          <Ionicons name="trash" size={32} color='#fff' />
+        </View>
+
+        {/* Lista de Produto */}
+
+        <View style={styles.footer}>
+
+        </View>
+
+      </ImageBackground>
+      <StatusBar style="light" backgroundColor='#000' />
+    </SafeAreaView>
   )
 }
 
