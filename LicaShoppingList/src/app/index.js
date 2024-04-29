cae7e4f4d5a51609cb845183a3b07b87ef07a189
lipeDaltoe <felipedaltoe9@gmail.com>
2024-04-29 11:58:04 -0300

    Finalizada a tela de splash

--- LicaShoppingList/src/app/index.js
+++ LicaShoppingList/src/app/index.js
@@ -1,15 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { Link } from "expo-router";
 
 export default function Welcome() {
   return (
     <View style={styles.container}>
-      <Text>Welcome!</Text>
-      <Link href={"/home"}>
-        <Text>Acessar</Text>
-      </Link>
-      <StatusBar style="auto" />
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../assets/avatar.png")}
+          style={styles.image} 
+        />
+      </View>
+      <View style={styles.content}>
+        <Text style={styles.title}>Lica Shopping List</Text>
+        <Text style={styles.text}>
+          Monte sua lista de compras e não esqueça mais o que precisa 
+          comprar ao sair de casa!!!
+        </Text>
+        <Link style={styles.button} href = {"/home"}>
+          <Text style={styles.buttonText}>Acessar</Text>
+        </Link>
+      </View>
+      <StatusBar style="light" backgroundColor='#000' />
     </View>
   );
 }
@@ -17,8 +29,50 @@ export default function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
+  },
+  imageContainer: {
+    flex: 2,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 230,
+    height: 230,
+    borderRadius: 115,
+    resizeMode: 'cover',
+  },
+  content: {
+    flex: 1,
+    backgroundColor: "#dadada",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingHorizontal: '5%',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 28,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  text: {
+    color: 'gray',
+    fontSize: 16,
+  },
+  button: {
+    position: 'absolute',
+    backgroundColor: '#000000',
+    borderRadius: 50,
+    bottom: '15%',
+    paddingVertical: 15,
+    width: '60%',
+    alignSelf: 'center',
+    textAlign: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: 'bold',
   },
 });
